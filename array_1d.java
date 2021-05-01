e7b9de3a888145f8a347618f0d8b2070afe123dd
llxSKyWALKeRxll <79057173+llxSKyWALKeRxll@users.noreply.github.com>
2021-05-02 01:05:53 +0530

    Update array_1d.java

--- array_1d.java
+++ array_1d.java
@@ -2,7 +2,7 @@
  * Class name: array_1d.
  * Purpose: 
  * @author Vansh Pratap Singh // Sky
- * @version 30/04/2021
+ * @version 30/04/2021 // v1
  */
 public class array_1d<T extends Comparable<T>>
 {
@@ -36,5 +36,59 @@ public class array_1d<T extends Comparable<T>>
         arr = (T[])new Comparable[n];
         ctr = 0;
         size = n;
+    }
+    /**
+     * This method returns the element at the specified index position, n.
+     * @param n The value stored at the index position n will be returned to the function call.
+     */
+    T return_at_index(int n)
+    {
+        return arr[n];
+    }
+    /**
+     * This method returns the index of the value passed as an argument (first instance).
+     */
+    int return_index(T n)
+    {
+        int res = -99;
+        for(int i=0; i<ctr; i++)
+        {
+            if(arr[i] == n) res = i;
+        }
+        return res;
+    }
+    /**
+     * This method will simply add the value that has been passed as an argument at the back of the array.
+     * @param n This value will be added to the array.
+     */
+    void add_element(T n)
+    {
+        arr[ctr] = n;
+        ctr++;
+    }
+    /**
+     * This method will add the value that has been passed as an argument at the front of the array, 
+     * and the rest of the elements will be shifted up by one, retaining the original order.
+     * @param n This value will be added to the array.
+     */
+    void add_element_front(T n)
+    {
+        ++ctr;
+        for(int i=ctr; i>0; i--) arr[i] = arr[i-1];
+        arr[0] = n;
+    }
+    /**
+     * This method prints all the elements currently present in the array, starting from the first index.
+     */
+    void print_all()
+    {
+        for(int i=0; i<ctr; i++) System.out.println("Element "+(i + 1)+": "+arr[i]);
+    }
+    /**
+     * This method returns the total no. of elements currently present in the array.
+     */
+    int total_elements()
+    {
+        return ctr;
     }
 }
